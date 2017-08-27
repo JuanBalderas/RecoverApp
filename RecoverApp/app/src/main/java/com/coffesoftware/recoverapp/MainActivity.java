77c397590ae8bd8a9de591dcb2263b4f763bde45
htino_opsjxn3 <htinoco.hernandez@outlook.es>
2017-08-27 15:15:35 -0500

    Prueba 1

--- RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/MainActivity.java
+++ RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    int var123344;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
