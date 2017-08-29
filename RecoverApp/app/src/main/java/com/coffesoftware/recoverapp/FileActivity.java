70e86c4205a97820c77107ff5396d2995d8c0d53
FranciscoMera <fcomera.ipn@gmail.com>
2017-08-28 23:56:34 -0500

    Cambio en actividad de archivos

--- RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/FileActivity.java
+++ RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/FileActivity.java
@@ -1,7 +1,11 @@
 package com.coffesoftware.recoverapp;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class FileActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class FileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file);
+    }
+
+    public void verWhatsApp(View view){
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeWhats);
+
     }
 }
