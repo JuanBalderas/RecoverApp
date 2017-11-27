6e99b31c756672a69a690f71f263f74be695d3d1
htino_opsjxn3 <htinoco.hernandez@outlook.es>
2017-11-27 09:49:11 -0600

    Mostrar ImEi y cambio de pantalla de login a resgitrar

--- RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/loginActivity.java
+++ RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/loginActivity.java
@@ -1,13 +1,23 @@
 package com.coffesoftware.recoverapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void logREG(View view) {
+
+        Intent objT = new Intent(loginActivity.this, registrarActivity.class);
+        startActivity(objT);
     }
 }
--- RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/registrarActivity.java
+++ RecoverApp/app/src/main/java/com/coffesoftware/recoverapp/registrarActivity.java
@@ -1,13 +1,52 @@
 package com.coffesoftware.recoverapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 public class registrarActivity extends AppCompatActivity {
+
+    TelephonyManager manager;
+    TextView txtmensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        txtmensaje = (TextView) findViewById(R.id.IMEI_DATOS);
+        manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        assert manager != null;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        @SuppressLint("HardwareIds") String builder = "Imei: " + manager.getDeviceId() + "\n" + "Nombre Operador" + manager.getNetworkOperatorName();
+                                                          //Obtiene el IMEI Y DATOS TELEFONO
+        txtmensaje.setText(builder);
+
     }
 }
